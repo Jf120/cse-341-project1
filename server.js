3feaada5cd7da6302173b8e93e9d4bc136690c62
Jf120 <jafc120@gmail.com>
2023-11-12 20:38:32 -0600

    Swagger install

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+  next();
+});
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
