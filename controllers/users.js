aec5d7f0d54db81ab09179f474802ab83a72d367
Jf120 <jafc120@gmail.com>
2023-11-15 22:54:06 -0600

    Route error handling added

--- controllers/users.js
+++ controllers/users.js
@@ -1,21 +1,49 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
+// const getAll = async (req, res) => {
+//     const result = await mongodb.getDatabase().db().collection('users').find();
+//     result.toArray().then((users) => {
+//         res.setHeader('Content-Type', 'application/json');
+//         res.status(200).json(users);
+//     });
+// };
+
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then((users) => {
+    try {
+        const result = await mongodb.getDatabase().db().collection('users').find();
+        const users = await result.toArray();
+
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users);
-    });
+    } catch (error) {
+        console.error('Error in getAll:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
 
+// const getSingle = async (req, res) => {
+//     const userId = new ObjectId(req.params.id);
+//     const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId });
+//     result.toArray().then((users) => {
+//         res.setHeader('Content-Type', 'application/json');
+//         res.status(200).json(users[0]);
+//     });
+// };
+
 const getSingle = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId });
-    result.toArray().then((users) => {
+    try {
+        const userId = new ObjectId(req.params.id);
+        const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId });
+        const users = await result.toArray();
+
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users[0]);
-    });
+        
+    } catch (error) {
+        console.error('Error in getSingle:', error);
+        res.status(500).json({ error: 'User not found' });
+    }
 };
 
 const createUser = async (req, res) => {
